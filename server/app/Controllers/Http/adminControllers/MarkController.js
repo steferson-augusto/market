6404c540152d457234382e71a963a3b5104b03ee
Steferson Augusto <steferson_a@hotmail.com>
2019-09-14 19:31:01 -0400

    Server - index de marcas

--- server/app/Controllers/Http/adminControllers/MarkController.js
+++ server/app/Controllers/Http/adminControllers/MarkController.js
@@ -1,28 +1,37 @@
 'use strict'
 
+const Mark = use("App/Models/Mark")
+const MessageError = use('./../Helpers/MessageError')
+
 class MarkController {
- 
-  async index ({ request }) {
-      return { success: true }
-  }
 
-  async create ({ request }) {
-  }
+    async index({ request, response }) {
+        try {
+            const { page, perPage } = request.only(['page', 'perPage'])
+            const data = await Mark.query().paginate(page, perPage)
+            return response.status(200).send(data)
+        } catch (error) {
+            return response.status(500).send({ error: MessageError.requestFail })
+        }
+    }
 
-  async store ({ request }) {
-  }
+    async create({ request }) {
+    }
 
-  async show ({ params, request }) {
-  }
+    async store({ request }) {
+    }
 
-  async edit ({ params, request }) {
-  }
+    async show({ params, request }) {
+    }
 
-  async update ({ params, request }) {
-  }
+    async edit({ params, request }) {
+    }
 
-  async destroy ({ params, request }) {
-  }
+    async update({ params, request }) {
+    }
+
+    async destroy({ params, request }) {
+    }
 }
 
 module.exports = MarkController
