007fccedec5f10e22b857c8f578c2753346398d8
Steferson Augusto <steferson_a@hotmail.com>
2019-09-14 19:30:20 -0400

    Web admin - buscando e listando marcas / mensagens de tradução da tabela

---
+++ web-admin/src/pages/App/Helpers/TableMessages.js
@@ -0,0 +1,22 @@
+const tableMessages = {
+    noData: 'Keine Daten verfügbar',
+};
+const editColumnMessages = {
+    addCommand: 'Neue Zeile',
+    editCommand: 'Bearbeiten',
+    deleteCommand: 'Entfernen',
+    commitCommand: 'Speichern',
+    cancelCommand: 'Abbrechen',
+};
+const groupingPanelMessages = {
+    groupByColumn: 'Ziehen Sie eine Spalte hierhin, um danach zu gruppieren',
+};
+const filterRowMessages = {
+    filterPlaceholder: 'Filter...',
+};
+
+export const pagingPanelMessages = {
+    showAll: 'Tudo',
+    rowsPerPage: 'Itens por página',
+    info: '{from} - {to} de {count}',
+}
--- web-admin/src/pages/App/Mark.js
+++ web-admin/src/pages/App/Mark.js
@@ -1,21 +1,80 @@
-import React from "react"
-import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-material-ui'
+import React, { useEffect } from "react"
+import Paper from '@material-ui/core/Paper'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { PagingState, IntegratedPaging, } from '@devexpress/dx-react-grid'
+import { Grid, Table, TableHeaderRow, PagingPanel } from '@devexpress/dx-react-grid-material-ui'
+
+import api from '../../services/api'
+import { pagingPanelMessages } from './Helpers/TableMessages'
 
 const Mark = () => {
+    const [loading, setLoading] = React.useState(true)
+    const [rows, setRows] = React.useState([])
+    const [query, setQuery] = React.useState({
+        total: 0,
+        page: 1,
+        perPage: 10,
+        sorting: 'name',
+        lastPage: null
+    })
+
+    useEffect(() => {
+        const fetchData = async () => getData()
+        fetchData()
+    }, [])
+
+    const pageSizes = [5, 10, 15, 20]
+    const sorting = ['id', 'name', 'active']
+    const columns = [
+        { name: 'id', title: 'ID' },
+        { name: 'name', title: 'Marca' },
+        { name: 'active', title: 'Ativo' },
+    ]
+
+    const getData = async () => {
+        setLoading(true)
+        try {
+            const queryParams = `page=${query.page}&perPage=${query.perPage}`
+            const { data: { data, ...rest } } = await api.get(`/admin/marks?${queryParams}`)
+            setRows(data)
+            setQuery(rest)
+            setLoading(false)
+            console.log(rows)
+            console.log(rest)
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
+
+    const onPageSizeChange = size => {
+        console.log('onPageSizeChange')
+        setQuery({ ...query, perPage: size })
+        getData()
+    }
+
     return (
-        <Grid
-            rows={[
-                { id: 0, product: 'DevExtreme', owner: 'DevExpress' },
-                { id: 1, product: 'DevExtreme Reactive', owner: 'DevExpress' },
-            ]}
-            columns={[
-                { name: 'id', title: 'ID' },
-                { name: 'product', title: 'Product' },
-                { name: 'owner', title: 'Owner' },
-            ]}>
-            <Table />
-            <TableHeaderRow />
-        </Grid>
+        <Paper style={{ position: 'relative' }}>
+            <Grid
+                rows={rows}
+                columns={columns}
+            >
+                <PagingState
+                    currentPage={query.page}
+                    onCurrentPageChange={() => console.log('onCurrentPageChange')}
+                    pageSize={query.perPage}
+                    onPageSizeChange={size => onPageSizeChange(size)}
+                />
+                <IntegratedPaging />
+                <Table />
+                <TableHeaderRow />
+                <PagingPanel
+                    pageSizes={pageSizes}
+                    messages={pagingPanelMessages}
+                />
+            </Grid>
+            {loading && <CircularProgress />}
+        </Paper>
     )
 }
 
