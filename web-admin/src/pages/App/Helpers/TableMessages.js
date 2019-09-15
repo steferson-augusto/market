a58dd79a9586c79bd3c14afcebe0bba6c6ca017c
Steferson Augusto <steferson_a@hotmail.com>
2019-09-15 02:15:41 -0400

    Web admin - criando estrutura da tabela

--- web-admin/src/pages/App/Helpers/TableMessages.js
+++ web-admin/src/pages/App/Helpers/TableMessages.js
@@ -1,22 +1,106 @@
-const tableMessages = {
-    noData: 'Keine Daten verfügbar',
-};
-const editColumnMessages = {
-    addCommand: 'Neue Zeile',
-    editCommand: 'Bearbeiten',
-    deleteCommand: 'Entfernen',
-    commitCommand: 'Speichern',
-    cancelCommand: 'Abbrechen',
-};
-const groupingPanelMessages = {
-    groupByColumn: 'Ziehen Sie eine Spalte hierhin, um danach zu gruppieren',
-};
-const filterRowMessages = {
-    filterPlaceholder: 'Filter...',
-};
+import React from 'react'
+import { DataTypeProvider } from '@devexpress/dx-react-grid'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import DeleteIcon from '@material-ui/icons/Delete'
+import EditIcon from '@material-ui/icons/Edit'
+import SaveIcon from '@material-ui/icons/Save'
+import CancelIcon from '@material-ui/icons/Cancel'
+
+// const tableMessages = {
+//     noData: 'Keine Daten verfügbar',
+// };
+// const editColumnMessages = {
+//     addCommand: 'Neue Zeile',
+//     editCommand: 'Bearbeiten',
+//     deleteCommand: 'Entfernen',
+//     commitCommand: 'Speichern',
+//     cancelCommand: 'Abbrechen',
+// };
+// const groupingPanelMessages = {
+//     groupByColumn: 'Ziehen Sie eine Spalte hierhin, um danach zu gruppieren',
+// };
+// const filterRowMessages = {
+//     filterPlaceholder: 'Filter...',
+// };
 
 export const pagingPanelMessages = {
     showAll: 'Tudo',
     rowsPerPage: 'Itens por página',
     info: '{from} - {to} de {count}',
 }
+
+const ActiveFormatter = ({ value }) => (
+    <b style={(value === 1) ? { color: 'green' } : (value === 0) ? { color: 'red' } : { color: 'grey' }}>
+        {(value === 1) ? 'Ativo' : (value === 0) ? 'Inativo' : 'Desconhecido'}
+    </b>
+)
+
+export const ActiveTypeProvider = props => (
+    <DataTypeProvider
+        formatterComponent={ActiveFormatter}
+        {...props}
+    />
+)
+
+export const AddButton = ({ onExecute }) => (
+    <div style={{ textAlign: 'center' }}>
+        <Button
+            color="primary"
+            onClick={onExecute}
+            title="Create new row"
+        >
+            Add
+      </Button>
+    </div>
+)
+
+export const EditButton = ({ onExecute }) => (
+    <IconButton onClick={onExecute} title="Editar">
+        <EditIcon />
+    </IconButton>
+)
+
+export const DeleteButton = ({ onExecute }) => (
+    <IconButton
+        onClick={() => {
+            // eslint-disable-next-line
+            if (window.confirm('Are you sure you want to delete this row?')) {
+                onExecute()
+            }
+        }}
+        title="Inativar"
+    >
+        <DeleteIcon />
+    </IconButton>
+)
+
+export const CommitButton = ({ onExecute }) => (
+    <IconButton onClick={onExecute} title="Save changes">
+        <SaveIcon />
+    </IconButton>
+)
+
+export const CancelButton = ({ onExecute }) => (
+    <IconButton color="secondary" onClick={onExecute} title="Cancel changes">
+        <CancelIcon />
+    </IconButton>
+)
+
+const commandComponents = {
+    "add": AddButton,
+    "edit": EditButton,
+    "delete": DeleteButton,
+    "commit": CommitButton,
+    "cancel": CancelButton,
+}
+
+export const Command = ({ id, onExecute }) => {
+    const CommandButton = commandComponents[id];
+    return (
+        <CommandButton
+            onExecute={onExecute}
+        />
+    )
+}
+
--- web-admin/src/pages/App/Mark.js
+++ web-admin/src/pages/App/Mark.js
@@ -1,15 +1,91 @@
-import React, { useEffect } from "react"
+import React, { useState, useEffect } from "react"
+import { PagingState, IntegratedPaging, EditingState } from '@devexpress/dx-react-grid'
+import {
+    Grid,
+    Table,
+    TableHeaderRow,
+    PagingPanel,
+    TableEditRow,
+    TableEditColumn,
+} from '@devexpress/dx-react-grid-material-ui'
+import { withStyles } from '@material-ui/core/styles'
+import Input from '@material-ui/core/Input'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
+import TableCell from '@material-ui/core/TableCell'
 import Paper from '@material-ui/core/Paper'
 import CircularProgress from '@material-ui/core/CircularProgress'
-import { PagingState, IntegratedPaging, } from '@devexpress/dx-react-grid'
-import { Grid, Table, TableHeaderRow, PagingPanel } from '@devexpress/dx-react-grid-material-ui'
 
 import api from '../../services/api'
-import { pagingPanelMessages } from './Helpers/TableMessages'
+import { pagingPanelMessages, ActiveTypeProvider, Command } from './Helpers/TableMessages'
+
+const styles = theme => ({
+    lookupEditCell: {
+        padding: theme.spacing(1),
+    },
+    dialog: {
+        width: 'calc(100% - 16px)',
+    },
+    inputRoot: {
+        width: '100%',
+    },
+})
+
+const availableValues = {
+    product: [],
+    region: [],
+    customer: [],
+}
+
+const LookupEditCellBase = ({ availableColumnValues, value, onValueChange, classes }) => (
+    <TableCell
+        className={classes.lookupEditCell}
+    >
+        <Select
+            value={value}
+            onChange={event => onValueChange(event.target.value)}
+            input={(
+                <Input
+                    classes={{ root: classes.inputRoot }}
+                />
+            )}
+        >
+            {availableColumnValues.map(item => (
+                <MenuItem key={item} value={item}>
+                    {item}
+                </MenuItem>
+            ))}
+        </Select>
+    </TableCell>
+);
+export const LookupEditCell = withStyles(styles, { name: 'ControlledModeDemo' })(LookupEditCellBase)
+
+// const Cell = (props) => {
+//     const { column } = props;
+//     if (column.name === 'discount') {
+//         return <ProgressBarCell {...props} />;
+//     }
+//     if (column.name === 'amount') {
+//         return <HighlightedCell {...props} />;
+//     }
+//     return <Table.Cell {...props} />;
+// };
+
+const EditCell = (props) => {
+    const { column } = props;
+    const availableColumnValues = availableValues[column.name];
+    if (availableColumnValues) {
+        return <LookupEditCell {...props} availableColumnValues={availableColumnValues} />;
+    }
+    return <TableEditRow.Cell {...props} />;
+}
 
 const Mark = () => {
-    const [loading, setLoading] = React.useState(true)
-    const [rows, setRows] = React.useState([])
+    const [loading, setLoading] = useState(true)
+    const [rows, setRows] = useState([])
+    const [addedRows, setAddedRows] = useState([])
+    const [rowChanges, setRowChanges] = useState({})
+    const [editingRowIds, getEditingRowIds] = useState([])
     const [query, setQuery] = React.useState({
         total: 0,
         page: 1,
@@ -17,19 +93,6 @@ const Mark = () => {
         sorting: 'name',
         lastPage: null
     })
-
-    useEffect(() => {
-        const fetchData = async () => getData()
-        fetchData()
-    }, [])
-
-    const pageSizes = [5, 10, 15, 20]
-    const sorting = ['id', 'name', 'active']
-    const columns = [
-        { name: 'id', title: 'ID' },
-        { name: 'name', title: 'Marca' },
-        { name: 'active', title: 'Ativo' },
-    ]
 
     const getData = async () => {
         setLoading(true)
@@ -47,10 +110,45 @@ const Mark = () => {
         }
     }
 
+    useEffect(() => {
+        getData()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    const pageSizes = [5, 10, 15, 20]
+    // const sorting = ['id', 'name', 'active']
+    const columns = [
+        { name: 'id', title: 'ID' },
+        { name: 'name', title: 'Marca' },
+        { name: 'active', title: 'Status' },
+    ]
+
     const onPageSizeChange = size => {
         console.log('onPageSizeChange')
         setQuery({ ...query, perPage: size })
         getData()
+    }
+
+    const changeAddedRows = (value) => {
+        const initialized = value.map(row => (Object.keys(row).length ? row : { city: 'Tokio' }));
+        setAddedRows(initialized);
+    };
+
+    const deleteRows = (deletedIds) => {
+        console.log('deleteRows')
+    }
+
+    const commitChanges = props => {
+        console.log(props)
+        if (props.added) {
+            console.log('added')
+        }
+        if (props.changed) {
+            console.log('changed')
+        }
+        if (props.deleted) {
+            console.log('deleted')
+        }
     }
 
     return (
@@ -65,9 +163,30 @@ const Mark = () => {
                     pageSize={query.perPage}
                     onPageSizeChange={size => onPageSizeChange(size)}
                 />
+                <ActiveTypeProvider for={["active"]} />
+
+                <EditingState
+                    editingRowIds={editingRowIds}
+                    onEditingRowIdsChange={getEditingRowIds}
+                    rowChanges={rowChanges}
+                    onRowChangesChange={setRowChanges}
+                    addedRows={addedRows}
+                    onAddedRowsChange={changeAddedRows}
+                    onCommitChanges={commitChanges}
+                />
                 <IntegratedPaging />
                 <Table />
                 <TableHeaderRow />
+                <TableEditRow
+                    cellComponent={EditCell}
+                />
+                <TableEditColumn
+                    width={170}
+                    showAddCommand={!addedRows.length}
+                    showEditCommand
+                    showDeleteCommand
+                    commandComponent={Command}
+                />
                 <PagingPanel
                     pageSizes={pageSizes}
                     messages={pagingPanelMessages}
